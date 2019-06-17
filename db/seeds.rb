78c75b92c1db491f3cced6cd89711646f61308bc
minhluong <minhluong@teqnological.asia>
2019-06-17 15:46:02 +0700

    Load Products

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,54 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Product.create([
+  {
+    name: 'LATEST MEN’S SNEAKER',
+    price: 25.0,
+    image: '/img/product/feature-product/f-p-1.jpg',
+    description: ''
+  },
+  {
+      name: 'RED WOMEN PURSES',
+      price: 25.0,
+      image: '/img/product/feature-product/f-p-2.jpg',
+      description: ''
+  },
+  {
+      name: 'MEN STYLIST SMART WATCH',
+      price: 25.0,
+      image: '/img/product/feature-product/f-p-3.jpg',
+      description: ''
+  },
+  {
+      name: 'NIKE LATEST SNEAKER',
+      price: 25.0,
+      image: '/img/product/new-product/n1.jpg',
+      description: ''
+  },
+  {
+      name: 'MEN’S DENIM JEANS',
+      price: 25.0,
+      image: '/img/product/new-product/n2.jpg',
+      description: ''
+  },
+  {
+      name: 'MEN’S SUMMER T-SHIRT',
+      price: 25.0,
+      image: '/img/product/new-product/new-product1.png',
+      description: ''
+  },
+  {
+      name: 'QUARTZ HAND WATCH',
+      price: 25.0,
+      image: '/img/product/new-product/n3.jpg',
+      description: ''
+  },
+  {
+      name: 'ADIDAS SPORT SHOE',
+      price: 25.0,
+      image: '/img/product/new-product/n4.jpg',
+      description: ''
+  },
+])
